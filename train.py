a1f018c55c1ed9152a308c5af586adbd8e825265
Prateek Vachher <vachh007@umn.edu>
2018-01-26 12:33:50 -0600

    Dynamic Training Steps

--- train.py
+++ train.py
@@ -1,7 +1,8 @@
 import os
+a = input('Enter Training Steps: ')
 os.system('python -m scripts.retrain \
   --bottleneck_dir=tf_files/bottlenecks \
-  --how_many_training_steps=500 \
+  --how_many_training_steps=' + str(a)+' \
   --model_dir=tf_files/models/ \
   --summaries_dir=tf_files/training_summaries/"mobilenet_0.50_224" \
   --output_graph=tf_files/retrained_graph.pb \
